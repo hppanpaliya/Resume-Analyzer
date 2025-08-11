7d48b2b2be38a34303cb970f91c695b464445fc3
hppanpaliya <hppanpaliya@gmail.com>
2025-08-11 02:52:06 -0400

    fix: Update server start scripts to reference index.js instead of server.js

--- ats-backend/index.js
+++ ats-backend/index.js
@@ -196,7 +196,7 @@ app.use((error, req, res, next) => {
 });
 
 // Serve React frontend
-app.use(express.static(path.join(__dirname, './build')));
+app.use(express.static(path.join(__dirname, 'build')));
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './build/index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
