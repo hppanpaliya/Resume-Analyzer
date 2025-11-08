22d1786e004ba907f4913ce203a59a1ee67cd6c4
hppanpaliya <hppanpaliya@gmail.com>
2025-11-07 21:58:58 -0500

    refactor: Remove ThemeToggle and useTheme from App component

--- ats-frontend/src/App.js
+++ ats-frontend/src/App.js
@@ -4,19 +4,11 @@ import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import Dashboard from './pages/Dashboard';
 import ProtectedRoute from './components/ProtectedRoute';
-import ThemeToggle from './components/ThemeToggle';
-import useTheme from './hooks/useTheme';
 
 function App() {
-  const { theme, toggleTheme } = useTheme();
-
   return (
     <BrowserRouter>
       <div className="min-h-screen">
-        <div className="fixed top-4 right-4 z-50">
-          <ThemeToggle theme={theme} toggleTheme={toggleTheme} />
-        </div>
-
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
