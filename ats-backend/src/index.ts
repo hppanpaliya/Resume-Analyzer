6c5b3b60b5e8410c436ba3befe6842c905156573
hppanpaliya <hppanpaliya@gmail.com>
2025-11-14 22:31:38 -0500

    chore: update dependencies and devDependencies in package.json
    - Bump @prisma/client from 6.17.0 to 6.19.0
    - Update axios from 1.11.0 to 1.13.2
    - Upgrade dotenv from 17.2.1 to 17.2.3
    - Upgrade express from 4.19.2 to 5.1.0
    - Update express-validator from 7.2.1 to 7.3.0
    - Upgrade mammoth from 1.10.0 to 1.11.0
    - Update openai from 5.12.1 to 6.9.0
    - Bump prisma from 6.17.0 to 6.19.0
    - Upgrade puppeteer from 24.23.0 to 24.30.0
    - Update uuid from 13.0.0 to 13.0.1
    - Upgrade zod from 3.25.76 to 4.1.12
    - Update @types/express from 5.0.3 to 5.0.5
    - Upgrade @types/node from 24.7.0 to 24.10.1
    - Update nodemon from 3.1.10 to 3.1.11
    
    fix: change route handling in index.ts to use regex for catch-all route

--- ats-backend/src/index.ts
+++ ats-backend/src/index.ts
@@ -44,8 +44,8 @@ app.get('/api/protected', authMiddleware, (req, res) => {
 
 app.use(express.static('build'));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build/index.html'));
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, "../build/index.html"));
 });
 
 app.listen(PORT, () => {
